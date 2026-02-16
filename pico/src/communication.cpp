91a9c40599836d00fd67de7d9815b1141048aed1
Huy <huy@hoang.dev>
2026-02-16 02:35:45 +0100

    low error rate without dma

--- pico/src/communication.cpp
+++ pico/src/communication.cpp
@@ -6,6 +6,12 @@ extern "C" {
 }
 
 #define FRAME_SIZE 12
+#define SPI_PORT spi0
+
+#define PIN_MOSI 16
+#define PIN_CS   17
+#define PIN_SCK  18
+#define PIN_MISO 19
 
 uint8_t rx_buf[FRAME_SIZE];
 int dma_chan;
@@ -17,6 +23,12 @@ uint32_t checksum_err = 0;
 uint32_t drop_err = 0;
 uint32_t payload_err = 0;
 uint8_t expected_id = 0;
+
+enum {
+    WAIT_A5,
+    WAIT_5A,
+    COLLECT
+} state = WAIT_A5;
 
 void reset_data() {
 	frames = 0;
@@ -34,21 +46,28 @@ void dma_init_spi() {
     channel_config_set_transfer_data_size(&cfg, DMA_SIZE_8);
     channel_config_set_read_increment(&cfg, false);
     channel_config_set_write_increment(&cfg, true);
-    channel_config_set_dreq(&cfg, spi_get_dreq(spi0, false));
+    channel_config_set_dreq(&cfg, spi_get_dreq(SPI_PORT, false));
 	
-    dma_channel_configure(
-        dma_chan,
-        &cfg,
-        rx_buf,
-        &spi_get_hw(spi0)->dr,
-        FRAME_SIZE,
-        false
-    );
+    dma_channel_configure(dma_chan, &cfg, rx_buf, &spi_get_hw(SPI_PORT)->dr, FRAME_SIZE, false);
 }
 
 void start_dma_receive() {
     dma_channel_set_write_addr(dma_chan, rx_buf, false);
     dma_channel_set_trans_count(dma_chan, FRAME_SIZE, true);
+}
+
+void print_successful_buffer() {
+	Serial.print(" Frame ");
+	Serial.print(rx_buf[2]);
+	Serial.print(" | Payload: ");
+
+	for (int i = 3; i < FRAME_SIZE; i++) {
+		Serial.print(rx_buf[i], HEX);
+		Serial.print(" ");
+	}
+
+	Serial.println();
+
 }
 
 void print_buffer() {
@@ -100,10 +119,10 @@ void communication_setup() {
     delay(2000);
 
     // SPI slave setup
-    spi_init(spi0, 50 * 1000 * 1000);
-    spi_set_slave(spi0, true);
+    spi_init(SPI_PORT, 50 * 1000 * 1000);
+    spi_set_slave(SPI_PORT, true);
 	spi_set_format(
-		spi0,
+		SPI_PORT,
 		8,              // bits
 		SPI_CPOL_0,
 		// phase needs to be 1 as seen here https://github.com/raspberrypi/pico-examples/issues/115#issuecomment-1051222803
@@ -111,10 +130,10 @@ void communication_setup() {
 		SPI_MSB_FIRST
 	);
 
-    gpio_set_function(16, GPIO_FUNC_SPI); // mosi
-    gpio_set_function(17, GPIO_FUNC_SPI); // chip select
-    gpio_set_function(18, GPIO_FUNC_SPI); // sclk
-    gpio_set_function(19, GPIO_FUNC_SPI); // miso
+    gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
+    gpio_set_function(PIN_CS, GPIO_FUNC_SPI);
+    gpio_set_function(PIN_SCK, GPIO_FUNC_SPI);
+    gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
 
     dma_init_spi();
 
@@ -122,61 +141,11 @@ void communication_setup() {
 }
 
 int idx = 0;
-void print_spi() {
-	if (!spi_is_readable(spi0))
-		return;
 
-	uint8_t b = spi_get_hw(spi0)->dr;
-	rx_buf[idx++] = b;
-
-	if (idx == FRAME_SIZE) {               // full frame received
-		idx = 0;
-		if (rx_buf[0] == 0xA5 && rx_buf[1] == 0x5A) {
-			// valid frame, print or process
-			Serial.print("Frame "); Serial.print(rx_buf[2]);
-			Serial.print(" | Payload: ");
-			for (int i=3;i<11;i++) Serial.print(rx_buf[i], HEX), Serial.print(" ");
-			Serial.println();
-		} else {
-		}
-	}
-}
-
-void write_dma() {
-	start_dma_receive();
-    dma_channel_wait_for_finish_blocking(dma_chan);
-
-    if (validate_frame(rx_buf)) {
-		Serial.print(sync_err);
-        Serial.print(" Frame ");
-        Serial.print(rx_buf[2]);
-        Serial.print(" | Payload: ");
-
-        for (int i = 3; i < FRAME_SIZE; i++) {
-			Serial.print(rx_buf[i], HEX);
-			Serial.print(" ");
-		}
-
-		Serial.println();
-
-	} else {
-
-
-		Serial.print(sync_err);
-		Serial.print(" Bad sync: ");
-		for (int i = 0; i < FRAME_SIZE; i++) {
-			Serial.print(rx_buf[i]);
-			Serial.print(", ");
-		}
-		Serial.println();
-	}
-}
-
-void check_stability() {
+void read_spi_old() {
 	static bool error = false;
 	while (spi_is_readable(spi0)) {
 		rx_buf[idx++] = spi_get_hw(spi0)->dr;
-
 		if (!error && idx == FRAME_SIZE) {
 			if (!validate_frame(rx_buf)) {
 				error = true;
@@ -188,53 +157,116 @@ void check_stability() {
 			error = false;
 		}
 	}
-
-	// start_dma_receive();
-	// dma_channel_wait_for_finish_blocking(dma_chan);
-
-	// validate_frame(rx_buf);
-
-    // static uint32_t last = 0;
-
-    // if (millis() - last > 1000) {
-    //     last = millis();
-		
-  //       Serial.println("-----");
-  //       Serial.print("Total Frames: "); Serial.println(frames);
-		// Serial.print("Frames since last print: "); Serial.println(frames - frames_last);
-  //       Serial.print("Sync: "); Serial.println(sync_err);
-  //       Serial.print("Checksum: "); Serial.println(checksum_err);
-  //       Serial.print("Drops: "); Serial.println(drop_err);
-  //       Serial.print("Payload: "); Serial.println(payload_err);
-		// float error_rate = (sync_err + checksum_err + drop_err + payload_err) / (float)frames;
-		// Serial.print("Error Rate: "); Serial.print(error_rate * 100, 4); Serial.println("%");
-
-		// if (frames - frames_last == 0)
-		// 	reset_data();
-
-		// frames_last = frames;
-    // }
 }
 
-uint32_t step = 0;
-double cum_time = 0;
+void read_spi() {
+	while (spi_is_readable(spi0)) {
+
+		uint8_t b = spi_get_hw(spi0)->dr;
+
+		switch(state) {
+			case WAIT_A5:
+				if (b == 0xA5) {
+					rx_buf[0] = b;
+					state = WAIT_5A;
+				}
+				break;
+			case WAIT_5A:
+				if (b == 0x5A) {
+					rx_buf[1] = b;
+					idx = 2;
+					state = COLLECT;
+				} else {
+					state = WAIT_A5;
+				}
+				break;
+			case COLLECT:
+				rx_buf[idx++] = b;
+				if (idx == FRAME_SIZE) {
+					if (frames < 100000)
+						expected_id = rx_buf[2];
+
+					if (!validate_frame(rx_buf))
+						expected_id = rx_buf[2] + 1;
+
+					state = WAIT_A5;
+					idx = 0;
+				}
+				break;
+		}
+	}
+}
+
+void write_dma() {
+	start_dma_receive();
+    dma_channel_wait_for_finish_blocking(dma_chan);
+    validate_frame(rx_buf);
+}
+
+
+void print_error_rate(uint32_t error) {
+	Serial.println((error / (float) frames)*100, 3);
+}
+
+void print_error_rates() {
+	Serial.print("Sync:     "); print_error_rate(sync_err);
+	Serial.print("Checksum: "); print_error_rate(checksum_err);
+	Serial.print("Drops:    "); print_error_rate(drop_err);
+	Serial.print("Payload:  "); print_error_rate(payload_err);
+
+// 	Serial.print("Sync:     "); Serial.println(sync_err);
+// 	Serial.print("Checksum: "); Serial.println(checksum_err);
+// 	Serial.print("Drops:    "); Serial.println(drop_err);
+// 	Serial.print("Payload:  "); Serial.println(payload_err);
+}
+
+void check_stability() {
+	read_spi();
+	// read_spi_old();
+	// write_dma();
+
+
+    static uint32_t last = 0;
+    if (millis() - last > 1000) {
+        last = millis();
+
+        Serial.println("--------");
+        // Serial.print("Total Frames: "); Serial.println(frames);
+		// Serial.print("Frames since last print: "); Serial.println(frames - frames_last);
+		print_error_rates();
+
+
+		// float error_rate = (sync_err + checksum_err + drop_err + payload_err) / (float)frames;
+		// Serial.print("Total Error Rate: "); Serial.print(error_rate * 100, 3); Serial.println("%");
+		// Serial.println("--------");
+
+		if (frames - frames_last == 0)
+			reset_data();
+
+		frames_last = frames;
+    }
+
+}
+
+// uint32_t step = 0;
+// double cum_time = 0;
 
 void communication_loop() {
-	static uint32_t last = 0;
-
-	int start = micros();
+	// static uint32_t last = 0;
+	// int start = micros();
 
 	// write_dma();
 	check_stability();
 
-	cum_time += micros() - start;
+	// cum_time += micros() - start;
 
-	if (millis() - last > 1000) {
-		last = millis();
+	// if (millis() - last > 1000) {
+	// 	last = millis();
 
-		Serial.print("loop takes on average: ");
-		Serial.print(cum_time / (double) step, 4);
-		Serial.println("us");
-	}
-	step++;
+	// 	Serial.print("loop takes on average: ");
+	// 	Serial.print(cum_time / (double) step, 3);
+	// 	Serial.println("us");
+	// 	cum_time = 0;
+	// }
+	// step++;
 }
